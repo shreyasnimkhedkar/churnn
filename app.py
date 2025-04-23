fb7dffcd931a212dc650ce3b82592bc5778bfe55
shreyasnimkhedkar <shreyasgithub1996@gmail.com>
2025-04-23 20:42:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+
+import streamlit as st
+
+st.title("📉 Customer Churn Prediction Web App")
+
+st.markdown("""
+Churn prediction is the process of identifying customers who are likely to stop using a product or service in the future.  
+Businesses use data analysis and machine learning techniques to anticipate customer behavior based on factors like purchase  
+history, engagement patterns, and feedback. By accurately predicting churn, companies can take proactive steps—like personalized  
+offers or improved customer service—to retain valuable customers and reduce revenue loss.
+""")
+
+# Native Streamlit button that opens a link
+st.link_button("Go to Customer churn Prediction", "https://churnnpre.streamlit.app/")
