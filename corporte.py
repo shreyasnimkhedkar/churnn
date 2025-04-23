518e3d36547977c3f1763de6b6b24b174a360e3c
shreyasnimkhedkar <shreyasgithub1996@gmail.com>
2025-04-23 23:45:16 +0530

    Add files via upload

---
+++ corporte.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+st.set_page_config(page_title="Customer Churn Prediction", layout="centered")
+
+st.title("CORPORATE BENIFITS")
+st.markdown("---")
+
+st.markdown("### Revenue Protection ")
+st.write("Churned customers = lost revenue. Predicting churn helps companies retain high-value customers, thus reducing financial losses and stabilizing recurring income, especially in subscription-based businesses.")
+
+st.markdown("### Smarter Marketing Spend")
+st.write("Instead of wasting budget on customers who are loyal or already lost, churn prediction helps focus efforts on at-risk customers, improving marketing ROI with targeted retention campaigns.")
+
+st.markdown("### Automation in Customer Success")
+st.write("Churn models can trigger automated alerts or customer retention workflows—such as discounts, offers, or outreach—ensuring faster action with less manual intervention.")
+
+st.markdown("### Customer Segmentation & Personalization")
+st.write("Corporations can use churn insights to segment customers by risk levels and tailor communication or services. This leads to more personalized experiences, which builds trust and satisfaction.")
+
+st.markdown("### Improved Forecasting")
+st.write("Understanding churn trends allows businesses to forecast future revenue, optimize resource allocation, and make data-driven strategic plans.")
+
+
+st.markdown("### Product & Service Optimization")
+st.write("Churn analysis reveals which features or pain points push customers away. This feedback loop helps in enhancing products, fixing service gaps, and innovating customer-centric solutions.")
+
+
+st.markdown("### Competitive Edge")
+st.write("Companies using churn prediction can outperform competitors by keeping more customers, offering better experiences, and improving brand loyalty—turning churn risk into a strategic advantage.")
+
