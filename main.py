79b874a2cf0996ffa2bc1cc76e7b33c7c1abd71e
shreyasnimkhedkar <shreyasgithub1996@gmail.com>
2025-04-23 23:47:42 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+
+
+
+import streamlit as st
+
+st.set_page_config(page_title="Customer Churn Prediction", layout="centered")
+
+st.title("*Indira Gandhi Polytechnic College*")
+st.markdown("---")
+
+st.markdown("### *NAME: Om Sahu*")
+st.markdown("### *Department: Computer Science Engineering*")
+st.markdown("### *PROJECT NAME: Customer Churn Prediction*")
+
+st.markdown("### *PROJECT KEYWORDS:*")
+st.markdown("""
+* _Go to customer churn prediction project_  
+* _Go to benefits of project_  
+* _Go to corporate benefit_
+""")
+
+
+st.link_button("Go to Customer churn Prediction", "https://churnnpre.streamlit.app/")
+st.link_button("Go to benifits of projct", "https://benifit-churn.streamlit.app/")
+st.link_button("go to corporate benifit", "https://corporate-benifits.streamlit.app/")
+
+
